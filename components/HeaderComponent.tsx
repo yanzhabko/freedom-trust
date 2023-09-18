258910885bc4a074c6882514f4f7c988fc422bb7
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2023-09-18 15:09:15 +0300

    update modal for mobile navigation

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { AiOutlineHeart, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { MdOutlineClose } from "react-icons/md";
 import classNames from "classnames";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 const navigationLink = [
   { title: "About", link: "/about" },
@@ -16,73 +16,101 @@ const navigationLink = [
 
 const HeaderComponent = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+    !isOpen
+      ? document.body.classList.add(style.scrolling)
+      : document.body.classList.remove(style.scrolling);
   };
 
   return (
-    <header className={style.placeholder}>
-      <div className={style.placeholder_up}>
-        <div className={style.container}>
-          <div className={style.help}>
-            <Link href="/donate" className={style.help__list}>
-              <AiOutlineHeart className={style.help__icon} />
-              Donate
-            </Link>
-            <div className={style.help__line}></div>
-            <Link href="/contact" className={style.help__list}>
-              <AiOutlineMail className={style.help__icon} />
-              Contact
-            </Link>
+    <header>
+      <div className={style.placeholder}>
+        <div className={style.placeholder_up}>
+          <div className={style.container}>
+            <div className={style.help}>
+              <Link href="/donate" className={style.help__list}>
+                <AiOutlineHeart className={style.help__icon} />
+                Donate
+              </Link>
+              <div className={style.help__line}></div>
+              <Link href="/contact" className={style.help__list}>
+                <AiOutlineMail className={style.help__icon} />
+                Contact
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={style.container}>
-        <nav className={style.navigation}>
-          <Link href="/" className={style.navigation__logo}>
-            Freedom Trust
-          </Link>
-          <ul className={style.navigation__list}>
-            {navigationLink.map((item, index) => (
-              <li className={classNames(style.navigation__item)} key={index}>
-                <Link href={item.link} className={classNames(pathname.startsWith(item.link) ? style.navigation__active : "", style.navigation__link)}>
-                  {item.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <button className={style.navigation__btn_burger} onClick={toggleMenu}>
-            {!isOpen ? (
-              <AiOutlineMenu className={style.navigation__btn_burger__icon} />
-            ) : (
-              <MdOutlineClose className={style.navigation__btn_burger__icon} />
-            )}
-          </button>
-        </nav>
-      </div>
-      {isOpen && (
-        <div
-          className={`${style.menu_wrapper} ${
-            isOpen ? style.menu_wrapper__bg : ""
-          }`}
-        >
-          <div className={style.menu_wrapper__placeholder}>
-            <div className={classNames(style.menu_wrapper__placeholder__list)}>
+        <div className={style.container}>
+          <nav className={style.navigation}>
+            <Link href="/" className={style.navigation__logo}>
+              Freedom Trust
+            </Link>
+            <ul className={style.navigation__list}>
               {navigationLink.map((item, index) => (
-                <div
-                  className={classNames(style.menu_wrapper__placeholder__item, ) }
-                  key={index}
-                >
+                <li className={classNames(style.navigation__item)} key={index}>
                   <Link
                     href={item.link}
-                    className={style.menu_wrapper__placeholder__link}
+                    className={classNames(
+                      pathname.startsWith(item.link)
+                        ? style.navigation__active
+                        : "",
+                      style.navigation__link
+                    )}
                   >
                     {item.title}
                   </Link>
-                </div>
+                </li>
               ))}
+            </ul>
+            <button
+              className={style.navigation__btn_burger}
+              onClick={toggleMenu}
+            >
+              <AiOutlineMenu className={style.navigation__btn_burger__icon} />
+            </button>
+          </nav>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className={style.modal}>
+          <div className={classNames(style.container, style.modal__container)}>
+            <div className={style.modal__btn}>
+              <button
+                className={style.navigation__btn_burger}
+                onClick={toggleMenu}
+              >
+                <MdOutlineClose
+                  className={style.navigation__btn_burger__icon}
+                />
+              </button>
+            </div>
+            <div className={style.modal__navigation}>
+              <div className={style.modal__list}>
+                <div className={style.modal__item}>
+                  <Link
+                    href="/"
+                    className={style.modal__link}
+                    onClick={toggleMenu}
+                  >
+                    Home
+                  </Link>
+                </div>
+                {navigationLink.map((item, index) => (
+                  <div
+                    key={index}
+                    className={style.modal__item}
+                    onClick={toggleMenu}
+                  >
+                    <Link href={item.link} className={style.modal__link}>
+                      {item.title}
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
