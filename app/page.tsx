2716641a27a4cdbe825ca2883bbe51f3c005e6e7
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2023-09-20 16:12:10 +0300

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import HeroSection from "@/sections/home_page/HeroSection"
+import BackgroundImages from "@/images/home_page/background.png"
 
 export default function Home() {
   return (
-    <div>
-      <HeroSection />
-    </div>
+    <>
+      <HeroSection image={BackgroundImages} title="Ukrainian Society for Children and Adults with Autims"/>
+    </>
   )
 }
--- sections/home_page/HeroSection.tsx
+++ sections/home_page/HeroSection.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles from "./HeroSection.module.scss";
+import { StaticImageData } from "next/image";
 
-const HeroSection = () => {
-	return (
-		<section>HeroSection</section>
-	)
+interface HeroProps {
+  image: StaticImageData;
+  title: string;
 }
 
-export default HeroSection
+const HeroSection: React.FC<HeroProps> = ({ image, title }) => {
+  const styleForBackground = {
+    backgroundImage: `url(${image.src})`,
+  };
+
+  return (
+    <section className={styles.hero}>
+      <div style={styleForBackground} className={styles.hero__background}>
+        <div className={styles.hero__container}>
+          <div className={styles.container}>
+            <div className={styles.hero__title}>{title}</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
