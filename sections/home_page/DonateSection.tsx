ba1ac2dd3dbdce308decfc35f0ce698ce9af005b
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2023-09-21 16:22:28 +0300

    add donate-section

--- sections/home_page/DonateSection.tsx
+++ sections/home_page/DonateSection.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import styles from "./DonateSection.module.scss";
+import Link from "next/link";
+import classNames from "classnames";
 
 const DonateSection = () => {
-	return (
-		<div>DonateSection</div>
-	)
-}
+  return (
+    <section className={styles.donate}>
+      <div className={classNames(styles.donate__container , styles.container)}>
+        <div className={styles.donate__images_container}>
+          <div className={styles.donate__images}></div>
+        </div>
+        <div className={styles.donate__box}>
+          <h1>Who we are?</h1>
+          <p className={styles.donate__text}>
+            Who wIt is a long established fact that a reader will be distracted
+            by the readable content of a page when looking at its layout. The
+            point of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English.e are?
+          </p>
+          <div className={styles.donate__btn}>
+            <Link href="/donate" className={styles.donate__btn__link}>
+              Donate now
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default DonateSection
+export default DonateSection;
