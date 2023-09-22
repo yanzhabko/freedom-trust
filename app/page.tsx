979c7cbf2f60069e2ffc992b2ca4b24b299ed15f
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2023-09-22 11:31:16 +0300

    add contact component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import HeroSection from "@/sections/home_page/HeroSection"
 import DonateSection from "@/sections/home_page/DonateSection"
+import ContactComponent from "@/components/ContactComponent"
 import BackgroundImages from "@/images/home_page/background.png"
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <div>
       <HeroSection image={BackgroundImages} title="Ukrainian Society for Children and Adults with Autims"/>
       <DonateSection />
+      <ContactComponent />
     </div>
   )
 }
