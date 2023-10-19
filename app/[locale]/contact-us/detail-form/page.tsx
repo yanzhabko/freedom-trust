646eee3dddbef56f1b285a145c862b7254648a69
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2023-10-19 21:20:10 +0300

    add contact-us section

---
+++ app/[locale]/contact-us/detail-form/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "@/sections/AuthSectionCommon.module.scss"
+import DetailFormSection from '@/sections/contact-us/DetailFormSection'
+
+const DetailForm = () => {
+	return (
+		<section className={styles.wrapper}>
+			<DetailFormSection />
+		</section>
+	)
+}
+
+export default DetailForm
